bd371c3e93573b3b9b67ddeadce78fbcc8a42b37
thesmallstar <manthan.surkar@gmail.com>
2019-07-08 22:03:09 +0530

    array

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
 print("Refresh Python Syntax FOR Ml.")
+import array as arr
+from array import *
+
+a = [1, 2, 3, 4]
+print(a[-1])
+
+#python array
+
+import array as arr
+from array import *
+
+a = arr.array('i', [2, 4, 6, 8])  # is is considered as type code
+
+print("First element:", a[0])
+print("Second element:", a[1])
+print("Second last element:", a[-1])
